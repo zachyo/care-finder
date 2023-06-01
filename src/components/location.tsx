5f5d570474da07103c834e7915e5b3c826ff4069
zacheusIsrael <segunzacheusi@gmail.com>
2023-06-01 02:25:32 +0100

    update

---
+++ src/components/location.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+interface LocationData {
+  principalSubdivision: string;
+  locality: string;
+  countryCode: string;
+}
+
+const Location: React.FC = () => {
+  const [data, setData] = useState<LocationData | null>(null);
+
+  const getLocation = () => {
+    const success = (position: GeolocationPosition) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      console.log(latitude, longitude);
+
+      const geoApiUrl = `https://api-bdc.net/data/reverse-geocode?latitude=${latitude}&longitude=${longitude}&localityLanguage=en&key=bdc_1d8b912bf33a4935905f692a75ffe550`;
+
+      axios.get(geoApiUrl).then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      });
+    };
+
+    const error = () => {
+      setData(null);
+      console.log("Unable to retrieve your location");
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error);
+  };
+
+  return (
+    <div>
+      <button className="location" onClick={getLocation}>
+        Get Location
+      </button>
+      {data && (
+        <ul>
+          {" "}
+          <li>
+            Location: {data.locality}, {data.principalSubdivision},{" "}
+            {data.countryCode}
+          </li>{" "}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Location;
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,13 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Location from "../components/location";
+import axios from "axios";
 
-const Homepage = () => {
-    const [hospitalList , setHospitalList] = useState()
+interface HospitalsData {
+  data: any
+}
+
+const Homepage: React.FC = () => {
+  const [hospitalList, setHospitalList] = useState<HospitalsData>();
+
+  const getHospitals = () => {
+    const hospitalApiUrl = `fetch from firebase`;
+
+    axios.get(hospitalApiUrl).then((response) => {
+      console.log(response.data);
+      setHospitalList(response.data);
+    });
+  };
+  useEffect(() => {
+    // getHospitals();
+  });
   return (
     <div>
       <h1 className="header">CareFinder</h1>
       <p>Find the nearest hospital to you</p>
       <input type="text" placeholder="Enter Address" />
       <div className="hospital-list">List of hospitals based on location</div>
+
+      <Location />
     </div>
   );
 };
