47a81c848c347eb792327fc7695de3ada324ecd4
zacheusIsrael <segunzacheusi@gmail.com>
2023-06-02 17:25:20 +0100

    Create customers.js

---
+++ src/customers.js
@@ -0,0 +1,13 @@
+export const customers = [{
+    id: 1,
+    name : 'John Doe',
+    email : 'johndoe@gmail.com',
+}, {
+    id: 2,
+    name : 'Jane Doe',
+    email : 'janedoe@gmail.com'
+}, {
+    id: 3,
+    name : 'John Smith',
+    email : 'johnsmith@gmail.com',
+}]
