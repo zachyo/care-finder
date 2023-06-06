d8d9b3ef8f4ea13ed5284c009aa8be17124b1459
zacheusIsrael <segunzacheusi@gmail.com>
2023-06-06 13:17:11 +0100

    Create firebase.utils.js

---
+++ src/firebase.utils.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  apiKey: "AIzaSyBJyhiyIrabNRYUsprVXfd8H_hc0j-MsA8",
+  authDomain: "carefinder-c0f35.firebaseapp.com",
+  projectId: "carefinder-c0f35",
+  storageBucket: "carefinder-c0f35.appspot.com",
+  messagingSenderId: "509123082325",
+  appId: "1:509123082325:web:a0d6c26f508ba952253cdc",
+  measurementId: "G-MZ6HRE9MXR",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+const analytics = getAnalytics(app);
+
+// export { auth, analytics };
