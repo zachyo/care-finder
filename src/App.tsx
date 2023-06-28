68acc9b4026352e77f7431c53b3ecd5e50256c44
zacheusIsrael <segunzacheusi@gmail.com>
2023-06-28 12:55:05 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage";
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./components/signup/sigunup";
+import SignIn from "./components/signin/signin";
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="signin" element={<SignIn />} />
+        {/* <Homepage /> */}
+      </Routes>
     </div>
   );
 }
