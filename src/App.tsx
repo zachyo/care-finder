d7a41cd35adda5adc2a89a4ab002a01eb4917227
zacheusIsrael <segunzacheusi@gmail.com>
2023-06-30 22:06:45 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Homepage from "./pages/homepage";
 import { Route, Routes } from "react-router-dom";
 import SignUp from "./components/signup/sigunup";
 import SignIn from "./components/signin/signin";
+import Hospitals from "./pages/hospitals";
+import Profile from "./pages/profile-page";
+import PrivateRoute from "./components/private-route";
 
 function App() {
   return (
@@ -12,6 +15,10 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="signup" element={<SignUp />} />
         <Route path="signin" element={<SignIn />} />
+        <Route path="search-hospital" element={<Hospitals />} />
+        {/* <Route path="profile" element={<PrivateRoute path="profile" element={<Profile/>}/>} /> */}
+
+        <Route path="profile" element={<Profile />} />
         {/* <Homepage /> */}
       </Routes>
     </div>
