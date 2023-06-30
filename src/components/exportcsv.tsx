b0b3175f14499b3e3900c70df649ffb17d295ece
zacheusIsrael <segunzacheusi@gmail.com>
2023-06-30 22:07:51 +0100

    hospital

--- src/components/exportcsv.tsx
+++ src/components/exportcsv.tsx
@@ -1,41 +1,40 @@
 import React, { useState } from "react";
 import "firebase/storage";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
-
-interface Customer {
-  id: number;
-  name: string;
-  email: string;
-    // [key: string]: number | string;
-}
+import { Hospital } from "../types/hospital";
 
 interface Props {
-  customers: Customer[];
+  data: Hospital[];
+  locality: string;
 }
 
-const ExportCustomersButton: React.FC<Props> = ({ customers }) => {
-  const filename : string = `${Date.now()}_hospital_list.csv`;
+const ExportCustomersButton: React.FC<Props> = ({ data, locality }) => {
+  const [loading, setLoading] = useState(false)
+  const filename: string = `${
+    locality ? locality : Date.now()
+  }_hospital_list.csv`;
   const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
-
+  const newData = data.filter((_, index) => index < 15);
+  console.log(locality)
   // Get a reference to the storage service, which is used to create references in your storage bucket
   const storage = getStorage();
 
   // Create a storage reference from our storage service
   const storageRef = ref(storage, "hospital_lists/" + filename);
   const handleExportClick = () => {
-    const csvData = generateCSVContent(customers);
+    setLoading(true)
+    const csvData = generateCSVContent(newData);
     const csvFile = new Blob([csvData], { type: "text/csv" });
-    
+
     //uupload to online storage
     uploadBytes(storageRef, csvFile).then((snapshot) => {
       console.log("Uploaded the CSV file!");
       //generate download link once uploaded
       downloadCsv();
     });
-
   };
 
-  const generateCSVContent = (data: Customer[]) => {
+  const generateCSVContent = (data: Hospital[]) => {
     const csvRows = [];
     const headers = Object.keys(data[0]);
 
@@ -43,7 +42,7 @@ const ExportCustomersButton: React.FC<Props> = ({ customers }) => {
 
     for (const row of data) {
       const values = headers.map((header) => {
-        const escaped = String(row[header as keyof Customer]).replace(
+        const escaped = String(row[header as keyof Hospital]).replace(
           /"/g,
           '\\"'
         );
@@ -59,20 +58,37 @@ const ExportCustomersButton: React.FC<Props> = ({ customers }) => {
       .then((url) => {
         console.log(url);
         setDownloadUrl(url);
+        setLoading(false)
       })
       .catch((error) => {
         // Handle any errors
         console.log(error);
       });
-  }
+  };
+  const generateShareableLink = (downloadUrl: string) => {
+    const subject = "Exported Hospital List";
+    const body = `Here is the link to download the exported hospital list: ${downloadUrl}`;
+    const mailtoLink = `mailto:?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`;
+
+    return mailtoLink;
+  };
 
   return (
     <div>
-      <button onClick={handleExportClick}>Export Customers as CSV</button>
-      {downloadUrl && (
+      <button onClick={handleExportClick}>Export Hospitals as CSV</button>
+      {downloadUrl && !loading && (
         <div>
           <a href={downloadUrl} download="hospital_list.csv">
             Download CSV
+          </a>
+          <a
+            href={generateShareableLink(downloadUrl)}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Share 
           </a>
         </div>
       )}
---
+++ src/components/hospital-list.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+// import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Hospital } from "../types/hospital";
+import useHospitalStore from "../utils/hospitalStore";
+
+interface Props {
+  data: Hospital[];
+}
+
+const HospitalListComponent: React.FC<Props> = ({ data }) => {
+  // useEffect(() => {
+  //   setGroupedPhotos(groupedPhotos);
+  // }, []);
+  const [page, setPage] = useState(1);
+
+  //paging system
+  const PER_PAGE = 20;
+  const total = data?.length;
+  const pages = Math.ceil(total / PER_PAGE);
+  const skip = page * PER_PAGE - PER_PAGE;
+
+  const Movies = data?.slice(skip, skip + PER_PAGE).map((hospital, index) => {
+    return (
+      <tr key={hospital.id}>
+        <td>{hospital.name}</td>
+        <td>{hospital.address ? `${hospital.address}` : ""}</td>
+        <td>{hospital.location}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div className="container flex flex-wrap justify-center ">
+      <table>
+        <thead>
+          <th>Name</th>
+          <th>Address</th>
+          <th>Location</th>
+        </thead>
+        <tbody>{Movies}</tbody>
+        {/* pagination */}
+        <>
+          <h3 className="pagination">
+            Pages: {data?.length > 0 ? page : 0} of {pages}
+          </h3>
+          <div className="page-btns">
+            <button
+              disabled={page <= 1}
+              onClick={() => setPage((prev) => prev - 1)}
+            >
+              Prev
+            </button>
+            {Array.from({ length: pages }, (_, index) => index + 1).map(
+              (each) => (
+                <span
+                  onClick={() => setPage(each)}
+                  key={each}
+                  style={page === each ? { backgroundColor: "#011ff3" } : {}}
+                >
+                  {/* {each} */}
+                </span>
+              )
+            )}
+            <button
+              disabled={page >= pages}
+              onClick={() => setPage((prev) => prev + 1)}
+            >
+              Next
+            </button>
+          </div>
+        </>
+      </table>
+    </div>
+  );
+};
+
+export default HospitalListComponent;
+
+// {
+//   data
+//     .filter((_, index) => index < 15)
+//     .map((hospital) => (
+//       <Link
+//         to={`/hospital/${hospital.id}`}
+//         key={hospital.id}
+//         className="mb-8 mx-2 hover:underline hover:scale-105 transition-all w-1/3 md:w-auto"
+//       >
+//         <h2 className="text-left mb-2">{hospital.name}</h2>
+//         {/* <div>
+//         <img
+//           src={hospital.icon_mask_base_uri}
+//           alt={hospital.name}
+//           className="rounded-xl hover:scale-110"
+//         />
+//       </div> */}
+//         <p>{hospital.address ? `Address - ${hospital.address}` : ""}</p>
+//       </Link>
+//     ));
+// }
