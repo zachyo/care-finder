0e41cbc7fd27ea472c974e38a695b7a60ceecb1e
zacheusIsrael <segunzacheusi@gmail.com>
2023-07-03 01:50:58 +0100

    Update hospitals.tsx

--- src/pages/hospitals.tsx
+++ src/pages/hospitals.tsx
@@ -6,6 +6,7 @@ import Location from "../components/location";
 import HospitalListComponent from "../components/hospital-list";
 import Spinner from "../components/spinner/spinner";
 import { Hospital } from "../types/hospital";
+import map from '../assets/image/Rectangle 56.png'
 import { Link } from "react-router-dom";
 
 const Hospitals = () => {
@@ -32,18 +33,25 @@ const Hospitals = () => {
   });
   return (
     <div className="hospitals">
-      <div className="search-button">
-        <input
-          type="text"
-          placeholder="Enter City..."
-          className="w-1/2 p-3 rounded-xl mt-6 placeholder:italic"
-          onChange={(event) => setSearchQuery(event.target.value)}
-          onKeyDown={handleKeyPress}
-        />
+      <div className="hospital-map">
+        {/* <img src={map} alt="map" className="absolute -z-10" /> */}
+        <div className="search-button flex gap-5 justify-center items-center pt-6">
+          <input
+            type="text"
+            placeholder="Enter City..."
+            className="w-1/2 p-3 rounded-xl border-2 border-deepBlueB placeholder:italic"
+            onChange={(event) => setSearchQuery(event.target.value)}
+            onKeyDown={handleKeyPress}
+          />
+          <Location />
+        </div>
       </div>
-      <Location />
 
-      <div className="hospital-list">List of hospitals</div>
+      <div className="hospital-list">
+        <h1 className="text-2xl font-semibold text-deepBlueB mt-12">
+          List of hospitals {locality ? `in ${locality}` : ""}
+        </h1>
+      </div>
       {loading && <Spinner />}
       {!loading && error && <p>{error.message}</p>}
       {data && (
