66efce19eb3c4026a413272707567966bc97b761
zacheusIsrael <segunzacheusi@gmail.com>
2023-07-03 01:57:17 +0100

    Update spinner.tsx

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -1,6 +1,6 @@
 const Spinner: React.FC = () => {
   return (
-    <div className="spinnerOverlay w-full flex justify-center items-center">
+    <div className="spinnerOverlay w-full h-[60vh] flex justify-center items-center">
       <div className="spinnerContainer inline-block w-[50px] h-[50px] border-8 border-solid border-loader rounded-[50%] animate-spin"></div>
     </div>
   );
