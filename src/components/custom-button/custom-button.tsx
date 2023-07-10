41f1ae0f353714e2ee19f78f5cbaa28475202ad0
zacheusIsrael <segunzacheusi@gmail.com>
2023-07-10 20:10:51 +0100

    Update custom-button.tsx

--- src/components/custom-button/custom-button.tsx
+++ src/components/custom-button/custom-button.tsx
@@ -1,9 +1,12 @@
-import React, { ChangeEvent, MouseEventHandler } from "react";
+import React from "react";
+import loaderIcon from "../../assets/icon/loader-icon.svg";
+
 
 interface Props {
   children: any;
   isGoogleSignIn?: boolean;
   inverted?: boolean;
+  isLoading?: boolean;
   type?: "button" | "submit" | "reset" | undefined;
   onclick: (event: React.MouseEvent<HTMLButtonElement>) => Promise<void>;
 }
@@ -11,11 +14,11 @@ const CustomButton: React.FC<Props> = ({
   children,
   isGoogleSignIn,
   inverted,
+  isLoading,
   type,
   onclick,
   ...otherProps
 }) => {
-
   return (
     <button
       className={`${inverted ? "inverted" : ""} ${
@@ -23,8 +26,17 @@ const CustomButton: React.FC<Props> = ({
       } custom-button font-bold rounded-xl bg-deepBlueB p-3 text-white border-2 hover:bg-white hover:text-deepBlueB hover:border-2 hover:border-deepBlueB`}
       {...otherProps}
       onClick={onclick}
+      disabled={isLoading}
     >
-      {children}
+      {isLoading ? (
+        <img
+          src={loaderIcon}
+          alt="loader"
+          className="animate-spin mx-auto"
+        />
+      ) : (
+        children
+      )}
     </button>
   );
 };
